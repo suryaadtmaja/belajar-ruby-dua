9ac81357197b617e3a1fdfa6f9195b3de7fb2f46
suryaadtmaja <suryaadtmaja48@yahoo.co.jp>
2017-08-30 08:59:55 +0700

    attr

---
+++ attr.rb
@@ -0,0 +1,27 @@
+class BonIver
+  attr_accessor :text
+  def initialize(text)
+     @text = text
+   end
+ 
+#  def text=(text) #setter
+#         @text = text
+#    end
+
+#   def text #getter
+#      @text
+#   end
+
+   def print
+     puts "mencetak : #{@text}"
+   end
+  end
+
+boniver = BonIver.new("Yaelah")
+boniver.print	
+
+boniver.text = "Bunga"
+boniver.print
+
+boniver.text = 'Ahihihi'
+puts boniver.text
